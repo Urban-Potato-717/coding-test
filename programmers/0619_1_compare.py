480ed4c4ba02b7caa5070e3fc67cf2ce4f0c37e6
Urban-Potato-717 <bumjun0302@gmail.com>
2026-06-19 15:27:47 +0900

    프로그레머스 Day1 Day2

---
+++ programmers/0619_1_compare.py
@@ -0,0 +1,11 @@
+# 숫자 비교하기
+def solution(num1,num2):
+    if num1 == num2: 
+        return 1
+    else:
+        return -1
+
+#혹은 삼항 연산자
+# 값1 if 조건 else 값2 - 값을 결정해주는 연산자
+def solution(num1,num2):
+    return (1 if num1 == num2 else -1)
